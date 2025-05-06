ea36f417470962a7eb641afe924c6f1ec6079621
InigoSanz <i.sanzdelg@gmail.com>
2025-05-06 20:06:06 +0200

    Cambiamos los links a la forma optima del ejercicio

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './shared/header/header.component';
-import { CardComponent } from './shared/card/card.component';
-import { SearchBarComponent } from './shared/search-bar/search-bar.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent, CardComponent, SearchBarComponent],
+  imports: [RouterOutlet, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
