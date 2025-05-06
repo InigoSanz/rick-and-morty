e137154f3652698e793b20a8d04e28049e52b680
InigoSanz <i.sanzdelg@gmail.com>
2025-05-06 19:55:43 +0200

    Añadimos componentes para cada modulo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { HeaderComponent } from './header/header.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { CardComponent } from './shared/card/card.component';
+import { SearchBarComponent } from './shared/search-bar/search-bar.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent],
+  imports: [RouterOutlet, HeaderComponent, CardComponent, SearchBarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
---
+++ src/app/shared/header/header.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-header',
+  imports: [],
+  templateUrl: './header.component.html',
+  styleUrl: './header.component.css',
+})
+export class HeaderComponent {
+  constructor(private _router: Router) {}
+
+  protected navigateTo(path: string) {
+    this._router.navigate([path]);
+  }
+}
